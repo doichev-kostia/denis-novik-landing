f12a3f955d38f392fe327985968fb64da18cf519
doichev-kostia <kostynd2004@gmail.com>
2021-06-11 23:11:15 +0300

    created tablet and desktop version

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -1,13 +1,25 @@
 const burgerMenu = document.querySelector(".menu__burger");
 const menu = document.querySelector(".menu__body");
+const tabletWidth = 768;
+const desktopWidth = 1024;
 
 burgerMenu.addEventListener("click", (e) => {
     const target = e.target;
-    const currentTarget = e.target
+    const currentTarget = e.target;
 
-    if (target === currentTarget || target.closest("button")){
+    if (target === currentTarget || target.closest("button")) {
         burgerMenu.classList.toggle("active");
         menu.classList.toggle("active");
-        document.body.classList.toggle("lock-screen")
+        document.body.classList.toggle("lock-screen");
     }
-})
+});
+
+window.addEventListener("resize", (e) => {
+    const targetWidth = e.target.innerWidth;
+    if (targetWidth > tabletWidth) {
+        document.body.classList.remove("lock-screen");
+        burgerMenu.classList.remove("active");
+        menu.classList.remove("active");
+    }
+
+});
